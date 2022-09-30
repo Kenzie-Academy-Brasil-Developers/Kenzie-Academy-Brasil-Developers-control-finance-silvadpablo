c6e65019fba99b93277debdb548a0a926e2bc8b9
silvadpablo <teacherpablod@hotmail.com>
2022-09-29 23:52:43 -0300

    working modal and card addition, no filtering and not dynamic yet

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,95 @@
-/* Desenvolva sua lógica aqui */
+let main = document.querySelector("main")
+
+
+
+function createInputNone () {
+    let div = document.createElement("div")
+    div.classList = "input-none flex flex-col items-center"
+
+    let h2 = document.createElement("h2")
+    h2.classList = "input-none-title text-1-bold"
+    h2.innerText = "Nenhum valor cadastrado"
+
+    let p = document.createElement("p")
+    p.classList = "input-none-text"
+    p.innerText = "Registrar novo valor"
+
+    div.append(h2, p)
+    main.append(div)
+}
+
+function addValueCard (selectedType, object) {
+    let totalSum = document.getElementById("entries-sum")
+    let sum = 0
+    object.forEach(element => {
+        let { id, value, categoryID } = element
+        let section = document.createElement("section")
+        section.classList = "flex justify-between items-center input-values"
+        section.id = `value-card-${id}`
+    
+        let divValue = document.createElement("div")
+        let pValue = document.createElement("p")
+        pValue.classList = "value-card-number"
+        pValue.innerText = `R$ ${value}`
+    
+        let divButtons = document.createElement("div")
+        divButtons.classList = "flex justify-between items-center"
+    
+        let pType = document.createElement("p")
+        pType.classList = "flex items-center btn btn-greylow btn-value"
+        
+        // categoryID = selectedType
+        if (valuesCategory.indexOf("Entrada") == categoryID) {
+            pType.innerText = "Entrada"
+            section.classList.add("Entrada")
+            pType.id = "Entrada"
+        } else if (selectedSaida = true) {
+            pType.innerText = "Saida"
+            pType.id = "Saída"
+            section.classList.add("Saída")
+        }
+    
+        let button = document.createElement("button")
+        button.classList = "btn icon"
+        button.id = "value-card-remove"
+        button.addEventListener("click", () => {
+            removeValueCard(element)
+            totalSum.innerText -= element.value
+        })
+    
+        divValue.append(pValue)
+        divButtons.append(pType, button)
+        section.append(divValue, divButtons)
+        main.append(section)
+        sum += value
+    });
+    totalSum.innerText = sum
+    filterValues()
+}
+
+function removeValueCard (element) {
+    let sectionValueCard = document.getElementById(`value-card-${element.id}`)
+    sectionValueCard.remove()    
+}
+
+function filterValues () {
+    let buttons = document.querySelectorAll(".btn-filter")
+    buttons.forEach(element => {
+        // console.log(element)
+        element.addEventListener("click", () => {
+            // console.log(event.target)
+            let selection = document.querySelectorAll(".input-values")
+            hideElement(selection)
+        })
+    })
+}
+
+function hideElement (elements) {
+    // console.log(elements)
+    // elements.forEach(element => {
+    //     if (element.id == "Entrada") {
+    //         console.log("deu")
+    //         document.querySelectorAll(".Saída").style.visibility = "hidden"
+    //     }
+    // })
+}
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,138 @@
-/* Desenvolva sua lógica aqui */
+let register = document.getElementById("buttonRegister")
+
+function openModal () {
+    register.addEventListener("click", () => {
+        createModal()
+        let close = document.getElementById("close-button")
+        let cancel = document.getElementById("cancel-button")
+        close.addEventListener("click", () => {
+            closeModal()
+        })
+        cancel.addEventListener("click", () => {
+            closeModal()
+        })
+    })
+}
+openModal()
+
+function closeModal () {
+    let modal = document.querySelector(".modal-wrap")
+    modal.remove()
+}
+
+function checkInput () {
+    const format = /[\d.]/
+    if (format.test(event.key) == false && event.keyCode != 8 && event.target.value !== undefined) {
+        event.target.value = event.target.value.slice(0, -1)
+    }
+    return insertedValue = event.target.value
+}
+
+function createModal () {
+    let section = document.createElement("section")
+    section.classList = "modal-wrap"
+
+    let modal = document.createElement("div")
+    modal.classList = "modal flex flex-col justify-between"
+
+    let header = document.createElement("header")
+    header.classList = "flex justify-between items-center"
+
+    let h2 = document.createElement("h2")
+    h2.classList = "title-2-bold"
+    h2.innerText = "Registro de valor"
+
+    let buttonClose = document.createElement("button")
+    buttonClose.id = "close-button"
+    buttonClose.classList = "btn btn-greylow"
+    buttonClose.innerText = "X"
+
+    let pDescription = document.createElement("p")
+    pDescription.classList = "text-1"
+    pDescription.innerText = "Digite o valor e em seguida aperte no botão referente ao tipo do valor"
+
+    let divInput = document.createElement("div")
+    divInput.classList = "flex flex-col"
+    
+    let inputLabel = document.createElement("label")
+    inputLabel.htmlFor = "valueModal"
+    
+    let divInputCurrency = document.createElement("div")
+    divInputCurrency.classList = "currency title-2-medium"
+
+    let inputValue = document.createElement("input")
+    inputValue.classList = "text-1 input"
+    inputValue.type = "text"
+    inputValue.id = "valueModal"
+    inputValue.name = "valueModal"
+    inputValue.placeholder = "00,00"
+    inputValue.addEventListener("keyup", () => {
+        checkInput()
+        
+    })
+
+    let divType = document.createElement("div")
+    divType.classList = "flex items-center modal-type"
+
+    let pType = document.createElement("p")
+    pType.innerText = "Tipo de valor"
+    pType.classList = "title-1-medium"
+
+    let buttonEntrada = document.createElement("button")
+    buttonEntrada.classList = "btn btn-outline btn-outline-selected"
+    buttonEntrada.id = "selected-entrada"
+    buttonEntrada.innerText = "Entrada"
+    let selectedType = 0
+    
+    let buttonSaida = document.createElement("button")
+    buttonSaida.classList = "btn btn-outline"
+    buttonEntrada.id = "selected-saida"
+    buttonSaida.innerText = "Saída"
+    
+    buttonSaida.addEventListener("click", () => {
+        if (buttonEntrada.classList == "btn btn-outline btn-outline-selected" && buttonSaida.classList == "btn btn-outline") {
+            buttonEntrada.classList.toggle("btn-outline-selected")
+            buttonSaida.classList.toggle("btn-outline-selected")
+            selectedType = 1
+        }
+    })
+
+    buttonEntrada.addEventListener("click", () => {
+        if (buttonSaida.classList == "btn btn-outline btn-outline-selected" && buttonEntrada.classList == "btn btn-outline") {
+            buttonEntrada.classList.toggle("btn-outline-selected")
+            buttonSaida.classList.toggle("btn-outline-selected")
+            selectedType = 0
+        }
+    })
+
+    let divButtons = document.createElement("div")
+    divButtons.classList = "flex justify-end modal-footer"
+    
+    let buttonCancelar = document.createElement("button")
+    buttonCancelar.classList = "btn btn-greylow"
+    buttonCancelar.innerText = "Cancelar"
+    buttonCancelar.id = "cancel-button"
+
+    let buttonInserir = document.createElement("button")
+    buttonInserir.classList = "btn btn-primary"
+    buttonInserir.innerText = "Inserir valor"
+    buttonInserir.id = "insert-value-card"
+    buttonInserir.addEventListener("click", () => {
+        if (typeof insertedValue === "undefined" || insertedValue == undefined){
+            alert("Insira um valor")
+        } else {
+            addValueCard(selectedType, insertedValues)
+            closeModal()
+        }
+    })
+
+    divButtons.append(buttonCancelar, buttonInserir)
+    divType.append(pType, buttonEntrada, buttonSaida)
+    divInput.append(inputLabel, divInputCurrency, inputValue)
+    header.append(h2, buttonClose)
+    modal.append(header, pDescription, divInput, divType, divButtons)
+    section.append(modal)
+    main.append(section)
+}
+
+// createModal()
